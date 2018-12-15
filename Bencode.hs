8b0e3c7857e9633c0a868f590fe82c6ff4981fe9
chrisg123 <chrisg_123@hotmail.com>
2018-12-15 11:49:31 -0500

    big refactor/simplification

--- Bencode.hs
+++ Bencode.hs
@@ -1,132 +1,61 @@
 module Bencode where
-
-data BType
-  = BTypeStr
-  | BTypeInt
-  | BTypeList
-  | BTypeDict
-  | BTypeInvalid
-  deriving (Eq, Show)
+import Data.Char (isDigit)
 
 data BValue
   = BStr String
   | BInt Int
   | BList [BValue]
-  | BDict [(String, BValue)]
+  | BDict [(BValue, BValue)]
+  deriving (Eq, Show)
 
-isDigit :: Char -> Bool
-isDigit x = x `elem` ['0' .. '9']
+encodeInt :: Int -> String
+encodeInt x = "i" ++ show x ++ "e"
 
-takeDigits :: String -> String
-takeDigits = takeWhile isDigit
-
-lastIndex :: Foldable t => t a-> Int
-lastIndex x = length x - 1
-
-getBType :: String -> Maybe BType
-getBType "" = Nothing
-getBType x =
-  case takeTaggedB x of
-    Just (t, _) -> Just t
-    _ -> Nothing
-
-getBValue :: String -> Maybe BValue
-getBValue "" = Nothing
-getBValue (x:xs) =
-  let b = takeB (x : xs)
-   in if b == ""
-        then Nothing
-        else Just (BStr b)
-      
--- |Find the first accurance of a bencoded element in a string.
-bfind :: String -> String
-bfind s =
-  let (i, len) = bfindPos s 0
-   in take len (drop i s)
-
--- |Find the position of the first accurance of a bencoded element in a string.
-bfindPos :: String -> Int -> (Int, Int)
-bfindPos "" _ = (0, 0)
-bfindPos s i
-  | i >= lastIndex s = (lastIndex s, 0)
+decodeInt :: String -> Maybe Int
+decodeInt "" = Nothing
+decodeInt (x:xs)
+  | x /= 'i' = Nothing
   | otherwise =
-    let b = takeB $ drop (min i $ lastIndex s) s
-     in if b /= ""
-          then (i, length b)
-          else bfindPos s (i + 1)
-
--- |Take a bencoded element from the head of a string.
-takeB :: String -> String
-takeB "" = ""
-takeB x =
-  let f (Just (_, s)) = s
-      f _ = ""
-       in f $ takeTaggedB x
-
--- |Take a bencoded element from the head of a string and tag it with its type 
-takeTaggedB :: String -> Maybe (BType, String)
-takeTaggedB "" = Nothing;
-takeTaggedB (x:xs)
-  | isDigit x = Just $ f (BTypeStr, maybe "" encStr . decStr)
-  | x == 'i' = Just $ f (BTypeInt, maybe "" encInt . decInt)
-  | x == 'l' = Just $ f (BTypeList, takeBCollection)
-  | x == 'd' = Just $ f (BTypeDict, takeBCollection)
-  | otherwise = Nothing
-  where
-    f (p, q) = (p, q (x : xs))
-
--- |Take all consecutive bencoded elements from the head of a string.
-takeBs :: String -> [String]
-takeBs "" = []
-takeBs s =
-  let b = takeB s
-   in if b == ""
-        then []
-        else b : takeBs (drop (length b) s)
-
--- |Take a bencoded list or dictionary element from the head of a string.
-takeBCollection :: String -> String
-takeBCollection "" = ""
-takeBCollection (x:xs)
-  | x == 'l' || x == 'd' =
-    let bs = concat $ takeBs xs
-        bsLength = length bs
-        validSuffix = (x : xs) !! (1 + bsLength) == 'e'
-     in if validSuffix
-          then take (bsLength + length (x : "e")) (x : xs)
-          else ""
-  | otherwise = "" 
-
-
-encInt :: Int -> String
-encInt x = "i" ++ show x ++ "e"
-
-decInt :: String -> Maybe Int
-decInt "" = Nothing
-decInt (x:xs)
-  | x == 'i' =
-    let dataString = takeDigits xs
+    let dataString = takeWhile (\y -> isDigit y || y == '-') xs
         validSuffix = (x : xs) !! (1 + length dataString) == 'e'
      in if validSuffix
           then Just $ read dataString
           else Nothing
-  | otherwise  = Nothing
            
-encStr :: String -> String
-encStr x = show (length x) ++ ":" ++ x
+encodeStr :: String -> String
+encodeStr x = show (length x) ++ ":" ++ x
 
-decStr :: String -> Maybe String
-decStr "" = Nothing
-decStr (x:xs)
-  | isDigit x =
-    let metaString = takeDigits (x : xs)
+decodeStr :: String -> Maybe String
+decodeStr "" = Nothing
+decodeStr (x:xs)
+  | not $ isDigit x = Nothing
+  | otherwise =
+    let metaString = takeWhile isDigit (x : xs)
         metaLength = length metaString
         dataLength = read metaString :: Int
         validSuffix = (x : xs) !! metaLength == ':'
      in if validSuffix
           then Just $ take dataLength (drop (metaLength + 1) (x:xs))
           else Nothing
-  | otherwise = Nothing
 
+decodeIntOrStr :: String -> Maybe BValue
+decodeIntOrStr x =
+  case decodeInt x of
+    Just y -> return $ BInt y
+    _ -> decodeStr x >>= \y -> return $ BStr y
 
--- decIntOrStr :: String -> Either Int String
+decodeSeqIntOrStr :: String -> [BValue] -> Maybe [BValue]
+decodeSeqIntOrStr "" xs  = return $ reverse xs
+decodeSeqIntOrStr x xs =
+  case decodeIntOrStr x of
+    Just (BInt p) ->
+      decodeSeqIntOrStr (drop (length $ encodeInt p) x) (BInt p : xs)
+    Just (BStr p) ->
+      decodeSeqIntOrStr (drop (length $ encodeStr p) x) (BStr p : xs)
+    _ -> Nothing
+    
+decodeLst :: String -> Maybe [BValue]
+decodeLst "" = Nothing
+decodeLst (x:xs)
+  | x /= 'l'= Nothing
+  | otherwise = decodeSeqIntOrStr (take (length xs - 1) xs) []
--- BencodeTests.hs
+++ BencodeTests.hs
@@ -3,207 +3,68 @@ module BencodeTests where
 import Bencode
 import Test.HUnit
 
-testBfindPosEmpty :: Test
-testBfindPosEmpty = TestCase $ assertEqual "Should return (0,0) for empty string." (0,0) (bfindPos "" 0)
+testEncodeInt :: Test
+testEncodeInt =  TestCase $assertEqual "Should return bencoded integer" "i123e" (encodeInt 123
+                                                                                )
+testDecodeInt :: Test
+testDecodeInt =
+  TestCase $ assertEqual "Should return a Just Int" (Just 3) (decodeInt "i3e")
 
-testBfindEmpty :: Test
-testBfindEmpty = TestCase $ assertEqual "Should return empty string given an empty string." "" (bfind "")
-
-testBfindPosNoMatch :: Test
-testBfindPosNoMatch =
-  TestCase $
-  assertEqual
-    "Should return (lastIndex str,0) for no match in string."
-    (3,0)
-    (bfindPos "asdf" 0)
-
-testBfindNoMatch :: Test
-testBfindNoMatch =
-  TestCase $
-  assertEqual
-    "Should return empty for no match in string."
-    ""
-    (bfind "asdf")
-
-testBfindPosFuzzyMatch :: Test
-testBfindPosFuzzyMatch =
-  TestCase $
-  assertEqual
-    "Should find bencoded string within a malformed bencoded string."
-    (4, 7)
-    (bfindPos "asdf5:hello" 0)
-
-testBfindFuzzyMatch :: Test
-testBfindFuzzyMatch =
-  TestCase $
-  assertEqual
-    "Should find bencoded string within a malformed bencoded data."
-    "5:hello"
-    (bfind "asdf5:hello")
-
-testBfindPosNoColon :: Test
-testBfindPosNoColon =
-  TestCase $
-  assertEqual
-    "Should skip forward if number not followed by colon."
-    (10, 8)
-    (bfindPos "asdf5Hello6:World!" 0)
-
-testBfindNoColon :: Test
-testBfindNoColon =
-  TestCase $
-  assertEqual
-    "Should skip forward if number not followed by colon."
-    "6:World!"
-    (bfind "asdf5Hello6:World!")    
-
-testBfindPosFirstWord :: Test
-testBfindPosFirstWord =
-  TestCase $
-  assertEqual
-    "Should only find the first word encountered."
-    (0, 7)
-    (bfindPos "5:Hello6:World!" 0)
-
-testBfindFirstWord :: Test
-testBfindFirstWord =
-  TestCase $
-  assertEqual
-    "Should only find the first word encountered."
-    "5:Hello"
-    (bfind "5:Hello6:World!")
-
-testBfindPosInt :: Test
-testBfindPosInt =
-  TestCase $
-  assertEqual "Should find bencoded integer position." (0, 5) (bfindPos "i123e" 0)
-
-testBfindPosFuzzyMatchInt :: Test
-testBfindPosFuzzyMatchInt =
-  TestCase $
-  assertEqual
-    "Should find bencoded integer position in malformed bencoded string."
-    (4, 5)
-    (bfindPos "asdfi123e" 0)
-
-testBfindPosOutOfBound :: Test
-testBfindPosOutOfBound =
-  TestCase $
-  assertEqual
-    "Should return (lastIndex str, 0) when i >= length str."
-    (14,0)
-    (bfindPos "5:Hello6:World!" 15)
+testDecodeNegInt :: Test
+testDecodeNegInt =
+  TestCase $ assertEqual "Should return a negative Just Int" (Just (-3)) (decodeInt "i-3e")
   
-testBfindPosStartPos :: Test
-testBfindPosStartPos =
+testEncodeStr :: Test
+testEncodeStr =  TestCase $assertEqual "Should return bencoded integer" "5:hello" (encodeStr "hello")
+
+testDecodeStr :: Test
+testDecodeStr =
   TestCase $
   assertEqual
-    "Should start search at i."
-    (7,8)
-    (bfindPos "5:Hello6:World!" 1)
-
-testBfindPosInvalidList :: Test
-testBfindPosInvalidList =
-  TestCase $
-  assertEqual
-    "Should return (lastIndex,0) for invalid list."
-    (3,0)
-    (bfindPos "lfda" 0)
-
-testTakeBsTwo :: Test
-testTakeBsTwo =
-  TestCase $
-  assertEqual
-    "Should take two consecutive b encoded elements."
-    ["5:Hello","6:World!"]
-    (takeBs "5:Hello6:World!")
-
-testBfindPosListOfTwo :: Test
-testBfindPosListOfTwo =
-  TestCase $
-  assertEqual
-    "Should return the position of a bencoded list of two elements."
-    (0,17)
-    (bfindPos "l5:Hello6:World!e" 0)
-
-testBfindPosDict :: Test
-testBfindPosDict =
-  TestCase $
-  assertEqual
-    "Should return the position of a bencoded dictionary."
-    (0,24)
-    (bfindPos "d3:cow3:moo4:spam4:eggse" 0)
-
-
-testGetBTypeNothing :: Test
-testGetBTypeNothing =
-  TestCase $
-  assertEqual "Should return Nothing" Nothing (getBType "asdf")
-
-testGetBTypeStr :: Test
-testGetBTypeStr =
-  TestCase $
-  assertEqual "Should return BTypeStr" (Just BTypeStr) (getBType "5:hello")
-
-testGetBTypeInt :: Test
-testGetBTypeInt =
-  TestCase $
-  assertEqual "Should return BTypeInt" (Just BTypeInt) (getBType "i123e")
-
-testGetBTypeList :: Test
-testGetBTypeList =
-  TestCase $
-  assertEqual "Should return BTypeList" (Just BTypeList) (getBType "li123e5:helloe")
-
-testGetBTypeDict :: Test
-testGetBTypeDict =
-  TestCase $
-  assertEqual "Should return BTypeDict" (Just BTypeDict) (getBType "di123e5:helloe")
-
-testDecStr :: Test
-testDecStr =
-  TestCase $
-  assertEqual
-    "Should return a haskell String"
+    "Should return a Just String"
     (Just "hello")
-    (decStr "5:hello")
+    (decodeStr "5:hello")
 
-testGetBType :: Test
-testGetBType =
-  TestLabel
-    "Test getBType"
-    (TestList
-       [ testGetBTypeStr
-       , testGetBTypeInt
-       , testGetBTypeList
-       , testGetBTypeDict
-       , testGetBTypeNothing
-       ])
+testDecodeIntOrStr1 :: Test
+testDecodeIntOrStr1 =
+    TestCase $
+  assertEqual
+    "Should return a Just String"
+    (Just $ BStr "hello")
+    (decodeIntOrStr "5:hello")
 
-testBfindPos :: Test
-testBfindPos = TestLabel "Test bfindPos." (  TestList
-    [ testBfindPosEmpty
-    , testBfindPosNoMatch
-    , testBfindPosFuzzyMatch
-    , testBfindPosNoColon
-    , testBfindPosFirstWord
-    , testBfindPosInt
-    , testBfindPosFuzzyMatchInt
-    , testBfindPosInvalidList
-    , testBfindPosStartPos
-    , testBfindPosOutOfBound
-    , testBfindPosListOfTwo
-    , testBfindPosDict
-    ] )
+testDecodeIntOrStr2 :: Test
+testDecodeIntOrStr2 =
+  TestCase $
+  assertEqual "Should return a Just Int" (Just $ BInt 3) (decodeIntOrStr "i3e")
 
-testBfind :: Test
-testBfind = TestLabel "Test bfind." (   TestList
-    [ testBfindEmpty
-    , testBfindNoMatch
-    , testBfindFuzzyMatch
-    , testBfindNoColon
-    , testBfindFirstWord
+testDecodeSeqIntOrStr :: Test
+testDecodeSeqIntOrStr =
+  TestCase $
+  assertEqual
+    "Should return list of BValues of Int or String"
+    (Just [BStr "hello", BStr "world!", BInt 123])
+    (decodeSeqIntOrStr "5:hello6:world!i123e" [])
+
+testDecodeLst :: Test
+testDecodeLst =
+  TestCase $
+  assertEqual
+    "Should return a list"
+    (Just [BStr "hello", BStr "world!", BInt 123])
+    (decodeLst "l5:hello6:world!i123ee")
+
+testDecode :: Test
+testDecode = TestLabel "Test decode." (   TestList
+    [ testEncodeInt
+    , testDecodeInt
+    , testDecodeNegInt    
+    , testDecodeStr
+    , testDecodeIntOrStr1
+    , testDecodeIntOrStr2
+    , testDecodeSeqIntOrStr
+    , testDecodeLst
     ] )
 
 main :: IO Counts
-main = runTestTT $ TestList [testTakeBsTwo, testBfindPos, testBfind, testGetBType, testDecStr]
+main = runTestTT $ TestList [testDecode]
