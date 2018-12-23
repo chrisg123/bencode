1cffce6b77ab52a94b4bee9e54a81f1a3c7c6554
chrisg123 <chrisg_123@hotmail.com>
2018-12-23 11:56:23 -0500

    encodes all bencoded elements; recursively if necessary.

--- Bencode.hs
+++ Bencode.hs
@@ -1,27 +1,29 @@
 module Bencode where
 import Data.Char (isDigit)
+import Data.Map.Strict as M
 
 data BValue
   = BStr String
   | BInt Int
-  | BList [BValue]
-  | BDict [(BValue, BValue)]
-  deriving (Eq, Show)
+  | BLst [BValue]
+  | BDct (Map String BValue)
+  deriving (Eq, Show, Ord)
 
 encodeInt :: Int -> String
 encodeInt x = "i" ++ show x ++ "e"
 
 decodeInt :: String -> Maybe Int
-decodeInt "" = Nothing
-decodeInt (x:xs)
-  | x /= 'i' = Nothing
+decodeInt (x0:x1:x2:xs)
+  | x0 /= 'i' ||
+      x1 /= '-' && not (isDigit x1) ||
+      x1 == '-' && not (isDigit x2) || last (x2 : xs) /= 'e' = Nothing
   | otherwise =
-    let dataString = takeWhile (\y -> isDigit y || y == '-') xs
-        validSuffix = (x : xs) !! (1 + length dataString) == 'e'
-     in if validSuffix
-          then Just $ read dataString
+    let d = x1 : takeWhile isDigit (x2 : xs)
+     in if length d == length (x1 : x2 : xs) - 1
+          then return (read d :: Int)
           else Nothing
-           
+decodeInt _ = Nothing
+
 encodeStr :: String -> String
 encodeStr x = show (length x) ++ ":" ++ x
 
@@ -44,18 +46,68 @@ decodeIntOrStr x =
     Just y -> return $ BInt y
     _ -> decodeStr x >>= \y -> return $ BStr y
 
-decodeSeqIntOrStr :: String -> [BValue] -> Maybe [BValue]
-decodeSeqIntOrStr "" xs  = return $ reverse xs
-decodeSeqIntOrStr x xs =
+decodeLstOrDct :: String -> Maybe BValue
+decodeLstOrDct x =
+  case decodeLst x of
+    Just y -> return $ BLst y
+    _ -> decodeDct x >>= \y -> return $ BDct y
+
+decodeAny :: String -> Maybe BValue
+decodeAny x =
   case decodeIntOrStr x of
+    Just y -> return y
+    _ -> decodeLstOrDct x >>= \y -> return y
+
+decodeSeq :: String -> [BValue] -> Maybe [BValue]
+decodeSeq "" xs  = return $ reverse xs
+decodeSeq x xs =
+  case decodeAny x of
     Just (BInt p) ->
-      decodeSeqIntOrStr (drop (length $ encodeInt p) x) (BInt p : xs)
+      decodeSeq (drop (length $ encodeInt p) x) (BInt p : xs)
     Just (BStr p) ->
-      decodeSeqIntOrStr (drop (length $ encodeStr p) x) (BStr p : xs)
+      decodeSeq (drop (length $ encodeStr p) x) (BStr p : xs)
+    Just (BLst p) -> decodeSeq (drop (length $ encodeLst p) x) (BLst p : xs)
+    Just (BDct p) -> decodeSeq (drop (length $ encodeDct p) x) (BDct p : xs)
     _ -> Nothing
     
 decodeLst :: String -> Maybe [BValue]
 decodeLst "" = Nothing
 decodeLst (x:xs)
-  | x /= 'l'= Nothing
-  | otherwise = decodeSeqIntOrStr (take (length xs - 1) xs) []
+  | x /= 'l' || last xs /= 'e' = Nothing
+  | otherwise = decodeSeq (take (length xs - 1) xs) []
+
+encodeLst :: [BValue] -> String
+encodeLst bs = 'l' : encodeCol (Left bs) "" ++ "e"
+
+decodeDct :: String -> Maybe (Map String BValue)
+decodeDct "" = Nothing
+decodeDct (x:xs)
+  | x /= 'd' || xs == "" || last xs /= 'e' = Nothing
+  | otherwise =
+    decodeSeq (take (length xs - 1) xs) [] >>= \y ->
+      let f (BStr b0:b1:bs) = (b0, b1) : f bs
+          f (BInt b0:b1:bs) = (show b0, b1) : f bs
+          f _ = []
+          keyValPairs = f y
+          complete = 2 * length keyValPairs == length y
+       in if complete
+            then Just $ M.fromList keyValPairs
+            else Nothing
+
+encodeDct ::  Map String BValue -> String
+encodeDct d = 'd' : encodeCol (Right (toList d)) "" ++ "e"
+
+encodeCol :: Either [BValue] [(String, BValue)] -> String -> String
+encodeCol (Left []) s = s
+encodeCol (Right []) s = s
+encodeCol x s =
+  let (key, val, xs) =
+        either
+          (\(w:ws) -> ("", w, Left ws))
+          (\((p, q):ws) -> (show (length p) ++ ":" ++ p, q, Right ws))
+          x
+   in case val of
+        BInt b -> encodeCol xs (s ++ key ++ encodeInt b)
+        BStr b -> encodeCol xs (s ++ key ++ encodeStr b)
+        BLst b -> encodeCol xs (s ++ key ++ encodeLst b)
+        BDct b -> encodeCol xs (s ++ key ++ encodeDct b)
--- BencodeTests.hs
+++ BencodeTests.hs
@@ -2,6 +2,7 @@ module BencodeTests where
 
 import Bencode
 import Test.HUnit
+import Data.Map 
 
 testEncodeInt :: Test
 testEncodeInt =  TestCase $assertEqual "Should return bencoded integer" "i123e" (encodeInt 123
@@ -13,7 +14,47 @@ testDecodeInt =
 testDecodeNegInt :: Test
 testDecodeNegInt =
   TestCase $ assertEqual "Should return a negative Just Int" (Just (-3)) (decodeInt "i-3e")
-  
+
+testDecodeInvalidInt1 :: Test
+testDecodeInvalidInt1 =
+  TestCase $ assertEqual "Should return Nothing for only negative sign" Nothing (decodeInt "i-e")  
+
+testDecodeInvalidInt2 :: Test
+testDecodeInvalidInt2 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i3-2e")  
+
+testDecodeInvalidInt3 :: Test
+testDecodeInvalidInt3 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "32e")
+
+testDecodeInvalidInt4 :: Test
+testDecodeInvalidInt4 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i32")
+
+testDecodeInvalidInt5 :: Test
+testDecodeInvalidInt5 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i-ae")
+
+testDecodeInvalidInt6 :: Test
+testDecodeInvalidInt6 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "iae")
+
+testDecodeInvalidInt7 :: Test
+testDecodeInvalidInt7 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "") 
+
+testDecodeInvalidInt8 :: Test
+testDecodeInvalidInt8 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i")
+
+testDecodeInvalidInt9 :: Test
+testDecodeInvalidInt9 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i-")
+
+testDecodeInvalidInt10 :: Test
+testDecodeInvalidInt10 =
+  TestCase $ assertEqual "Should return Nothing for invalid bencoded integer" Nothing (decodeInt "i3")  
+
 testEncodeStr :: Test
 testEncodeStr =  TestCase $assertEqual "Should return bencoded integer" "5:hello" (encodeStr "hello")
 
@@ -44,27 +85,122 @@ testDecodeSeqIntOrStr =
   assertEqual
     "Should return list of BValues of Int or String"
     (Just [BStr "hello", BStr "world!", BInt 123])
-    (decodeSeqIntOrStr "5:hello6:world!i123e" [])
+    (decodeSeq "5:hello6:world!i123e" [])
 
-testDecodeLst :: Test
-testDecodeLst =
+testDecodeLst1 :: Test
+testDecodeLst1 =
   TestCase $
   assertEqual
     "Should return a list"
     (Just [BStr "hello", BStr "world!", BInt 123])
     (decodeLst "l5:hello6:world!i123ee")
 
+testDecodeLst2 :: Test
+testDecodeLst2 =
+  TestCase $
+  assertEqual
+    "Should return a nested list"
+    (Just [BLst [BStr "hello"]])
+    (decodeLst "ll5:helloee")
+
+testEncodeLst1 :: Test
+testEncodeLst1 =
+  TestCase $
+  assertEqual
+    "Should return encoded list."
+  "l5:hello6:world!e"
+    (encodeLst [BStr "hello", BStr "world!"])
+
+testEncodeLst2 :: Test
+testEncodeLst2 =
+  TestCase $
+  assertEqual
+    "Should return encoded list."
+  "l5:hello6:world!li123e3:abcee"
+    (encodeLst [BStr "hello", BStr "world!", BLst [BInt 123, BStr "abc"]])
+
+testDecodeLst :: Test
+testDecodeLst =
+  TestLabel
+    "Test pairs."
+    (TestList [testDecodeLst1, testDecodeLst2])
+
+    
+testDecodeDct1 :: Test
+testDecodeDct1 = TestCase $ assertEqual "Should return nothing for invalid dictionary" Nothing (decodeDct "")
+
+testDecodeDct2 :: Test
+testDecodeDct2 = TestCase $ assertEqual "Should return nothing for invalid dictionary" Nothing (decodeDct "d")
+
+testDecodeDct3 :: Test
+testDecodeDct3 = TestCase $ assertEqual "Should return nothing for invalid dictionary" Nothing (decodeDct "d5:helloe")
+
+testDecodeDct4 :: Test
+testDecodeDct4 =
+  TestCase $
+  assertEqual
+    "Sould return a Data.Map.fromList"
+    (Just (Data.Map.fromList [("hello", BStr "world!")]))
+    (decodeDct "d5:hello6:world!e")
+
+testDecodeDct5 :: Test
+testDecodeDct5 =
+  TestCase $
+  assertEqual
+    "Sould return nested Data.Map.fromList"
+    (Just (Data.Map.fromList [("key", BDct (Data.Map.fromList [("hello", BStr "world!")]))]))
+    (decodeDct "d3:keyd5:hello6:world!ee")
+
+testEncodeDct :: Test
+testEncodeDct =
+  TestCase $
+  assertEqual
+    "Should return a bencoded dictionary."
+    "d4:key16:value14:key26:value2e"
+    (encodeDct (fromList [("key1", BStr "value1"), ("key2", BStr "value2")]))
+
+testDecodeDct :: Test
+testDecodeDct =
+  TestLabel
+    "Test pairs."
+    (TestList
+       [ testDecodeDct1
+       , testDecodeDct2
+       , testDecodeDct3
+       , testDecodeDct4
+       , testDecodeDct5
+       ])
+
 testDecode :: Test
 testDecode = TestLabel "Test decode." (   TestList
-    [ testEncodeInt
-    , testDecodeInt
-    , testDecodeNegInt    
+    [ testDecodeInt
+    , testDecodeNegInt
+    , testDecodeInvalidInt1
+    , testDecodeInvalidInt2
+    , testDecodeInvalidInt3
+    , testDecodeInvalidInt4
+    , testDecodeInvalidInt5
+    , testDecodeInvalidInt6
+    , testDecodeInvalidInt7
+    , testDecodeInvalidInt8
+    , testDecodeInvalidInt9
+    , testDecodeInvalidInt10    
     , testDecodeStr
     , testDecodeIntOrStr1
     , testDecodeIntOrStr2
     , testDecodeSeqIntOrStr
     , testDecodeLst
+    , testDecodeDct
+    ] )
+
+
+testEncode :: Test
+testEncode = TestLabel "Test encode." (   TestList
+    [ testEncodeInt
+    , testEncodeLst1
+    , testEncodeLst2
+    , testEncodeDct
     ] )
 
 main :: IO Counts
-main = runTestTT $ TestList [testDecode]
+main = runTestTT $ TestList [testEncode, testDecode]
